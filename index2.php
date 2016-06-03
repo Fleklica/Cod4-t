7b82ddf7ef57cfadf60fa58633f174ab627628f4
Fleklica <andrej.penezic@skole.hr>
2016-06-03 09:33:37 +0200

    Dodo index2.php ali jos uvijek nerade delet i edit.

---
+++ index2.php
@@ -0,0 +1,84 @@
+<?php require('check.php'); ?>
+<?php session_start(); ?>
+<?php require('db.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Automobili</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/template.css" rel="stylesheet">
+    <![endif]-->
+</head>
+
+<body>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">Cod4-tournament</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="index.php">Početna</a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</nav>
+
+<div class="container">
+
+    <div class="starter-template">
+        <h1>Cod4-tournament</h1>
+        <p class="lead">Cod4-tournament</p>
+    </div>
+
+    <div>
+        <p>
+            <?php
+            echo(session_id());
+            echo($_SESSION['info']);
+
+            $sql = "SELECT * FROM Ekipe";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                // output data of each row
+                echo('<table>');
+                echo('<tr><th>id</th><th>Ime clana</th><th>Prezime clana</th><th>uloga</th><th>Ime ekipe</th></tr>');
+                while($row = $result->fetch_assoc()) {
+                    echo('<tr>');
+                    echo('<td>' . $row['id'] . '</td>');
+                    echo('<td>' . $row['Ime_clana'] . '</td>');
+                    echo('<td>' . $row['Prezime_clana'] . '</td>');
+                    echo('<td>' . $row['uloga'] . '</td>');
+                    echo('<td>' . $row['Ime_ekipe'] . '</td>');
+                    echo('<td>' . '<a href="delete.php?id=' . $row['id'] . '"><img src="img/delete.gif"></a>' . '</td>');
+                    echo('<td>' . '<a href="edit.php?id=' . $row['id'] . '"><img src="img/edit.gif"></a>' . '</td>');
+                    echo('</tr>');
+                }
+                echo('</table>');
+            } else {
+                echo "Nema clanova";
+            }
+            $conn->close();
+            ?>
+        </p>
+    </div>
+
+</div><!-- /.container -->
+
+
+</body>
+</html>
